ace2fc93ca180af108eb6b9afa44dbdc0ce758e8
Sergei Giniatulin <kornburn@gmail.com>
2025-10-09 01:38:01 +0300

    Vibe.D new views
    Views were updated with the same logic as for Serverino

--- source/app.d
+++ source/app.d
@@ -1,6 +1,13 @@
-import vibe.vibe;
+import vibe.http.server;
+import vibe.http.router;
+import vibe.http.status;
+import vibe.http.fileserver;
+
+import vibe.core.log;
+import vibe.core.core;
 
 import tailwind;
+import std;
 
 void main()
 {
@@ -20,9 +27,13 @@ void main()
 	settings.bindAddresses = ["::1", "127.0.0.1"];
 
 	auto router = new URLRouter;
-	router.get("/", &index);
-	router.get("/login-form", &loginForm);
-	router.get("/show-form", &showForm);
+	router
+	    .get("/", &index)
+		.get("/login-form", &loginForm)
+		.get("/show-form", &showForm)
+		.post("/submit-talk", &submitTalk);
+
+	router.rebuild();
 	router.get("*", serveStaticFiles("public"));
 
 	auto listener = listenHTTP(settings, router);
@@ -38,7 +49,11 @@ void main()
 
 void index(HTTPServerRequest req, HTTPServerResponse res)
 {
-	res.render!("index.dt");
+    bool isHTMX = req.headers.get("HX-Request", "") == "true" ? true : false;
+    if (isHTMX)
+        res.render!("index_dynamic.dt");
+    else
+        res.render!("index.dt");
 }
 
 void loginForm(HTTPServerRequest req, HTTPServerResponse res)
@@ -49,4 +64,33 @@ void loginForm(HTTPServerRequest req, HTTPServerResponse res)
 void showForm(HTTPServerRequest req, HTTPServerResponse res)
 {
 	res.render!("form.dt");
+}
+
+void submitTalk(HTTPServerRequest req, HTTPServerResponse res)
+{
+    writeln(req.form);
+    auto name = req.form.get("name");
+    auto theme = req.form.get("theme");
+    auto experience = req.form.get("experience");
+    auto durationStr = req.form.get("duration");
+
+    // Basic validation
+    if (name.length == 0 || theme.length == 0 || experience.length == 0 || durationStr.length == 0) {
+        res.statusCode = HTTPStatus.badRequest;
+        res.writeBody("All fields are requried", "text/plain");
+        return;
+    }
+
+    int duration;
+    try {
+        duration = to!int(durationStr);
+    } catch (ConvException) {
+        res.statusCode = HTTPStatus.badRequest;
+        res.writeBody("Duration must be a number", "text/plain");
+        return;
+    }
+
+    info("Received talk proposal: ", name, ", theme: ", theme, ", experience: ", experience, ", duration: ", duration);
+
+    res.render!("form_finished.dt");
 }
